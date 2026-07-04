12a90b19de17adfae5456f4d9473952983280e85
dainagon aleo <140230026+dainagon89@users.noreply.github.com>
2026-07-04 15:53:05 +0900

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,7 +1,16 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-  serverExternalPackages: ['@coinbase/agentkit', '@coinbase/agentkit-langchain'],
+  experimental: {
+    serverComponentsExternalPackages: [
+      '@coinbase/agentkit',
+      '@coinbase/agentkit-langchain',
+      '@coinbase/cdp-sdk',
+      'langchain',
+      '@langchain/core',
+      '@langchain/langgraph',
+      '@langchain/openai',
+    ],
+  },
 };
 
 module.exports = nextConfig;
